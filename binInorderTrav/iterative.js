506a27d6e6956763ab7be9271fb12d37d2eb031e
ansel brandt <ansel@anselbrandt.com>
2022-08-20 23:14:53 -0400

    Update

---
+++ binInorderTrav/iterative.js
@@ -0,0 +1,47 @@
+const root = {
+  val: 1,
+  left: {
+    val: 2,
+    left: {
+      val: 4,
+      left: null,
+      right: null,
+    },
+    right: {
+      val: 5,
+      left: null,
+      right: null,
+    },
+  },
+  right: {
+    val: 3,
+    left: {
+      val: 6,
+      left: null,
+      right: null,
+    },
+    right: {
+      val: 7,
+      left: null,
+      right: null,
+    },
+  },
+};
+
+var inorderTraversal = function (root) {
+  let current = root;
+  const stack = [];
+  const result = [];
+  while (current || stack.length) {
+    while (current) {
+      stack.push(current);
+      current = current.left;
+    }
+    current = stack.pop();
+    result.push(current.val);
+    current = current.right;
+  }
+  return result;
+};
+
+console.log(inorderTraversal(root));
