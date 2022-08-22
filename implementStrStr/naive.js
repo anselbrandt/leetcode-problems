b291b1ccfad1f8d857e506e1a6ef79e98528347a
ansel brandt <ansel@anselbrandt.com>
2022-08-22 05:17:26 -0400

    Update

--- implementStrStr/naive.js
+++ implementStrStr/naive.js
@@ -9,6 +9,7 @@ const needle = "acacagt";
 //              a  c  a  c  a  g  t
 const prefix = [0, 0, 1, 2, 3, 0, 0];
 
+// add var to indicate full match
 var strStr = function (haystack, needle) {
   if (needle === "") return -1;
   let index = -1;
@@ -16,12 +17,13 @@ var strStr = function (haystack, needle) {
   let j = 0;
   while (i < haystack.length) {
     const match = haystack[i] === needle[j];
-    console.log(i, j, haystack[i], needle[j], match);
-    if (match && j === 0) index = i;
+    if (match && j === 0 && index === -1) index = i;
+    if (match && j === needle.length - 1) return index;
     if (match) {
       i++;
       j++;
     } else {
+      index = -1;
       j = 0;
       i++;
     }
