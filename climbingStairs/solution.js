30c3aaf6f202f7a8e442fbd9bd25c60b04561fbc
ansel brandt <ansel@anselbrandt.com>
2022-08-24 21:35:49 -0400

    Update

---
+++ climbingStairs/solution.js
@@ -0,0 +1,15 @@
+const search = (steps, sum = 0, results = new Map()) => {
+  if (sum > steps) return 0;
+  if (sum === steps) return 1;
+  if (results.has(sum)) return results.get(sum);
+  const result =
+    search(steps, sum + 1, results) + search(steps, sum + 2, results);
+  results.set(sum, result);
+  return result;
+};
+
+var climbStairs = function (n) {
+  return search(n);
+};
+
+console.log(climbStairs(5));
